8d82cb8c687f3396f6835904593b9ce2787669ea
Paulina282000 <paulina.282000@gmail.com>
2025-03-31 00:15:18 -0300

    conexion con mongo parte 1

--- main.go
+++ main.go
@@ -1,96 +1,69 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type ToDo struct {
-	ID        int    `json:"id"`
+	ID        int    `json:"_id" bson:"_id"`
 	Completed bool   `json:"completed"`
 	Body      string `json:"body"`
 }
 
-var todos []ToDo // Definición global de todos, no es necesario redeclararla dentro de main
+var collection *mongo.Collection
 
 func main() {
 	fmt.Println("Hello, World!")
-
-	app := fiber.New()
 
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	PORT := os.Getenv("PORT")
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client, err := mongo.Connect(context.Background(), clientOptions)
 
-	// Definir la ruta raíz '/'
-	app.Get("/api/todos", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(todos)
-	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Ruta POST para crear un nuevo ToDo
-	app.Post("/api/todos", func(c *fiber.Ctx) error {
-		todo := &ToDo{} // Se usa 'ToDo' en lugar de 'Todo'
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		// Analiza el cuerpo de la solicitud y lo almacena en la variable 'todo'
-		if err := c.BodyParser(todo); err != nil {
-			return err
-		}
+	fmt.Printf("Connected to MongoDB ATLAS")
 
-		// Verificar que el campo 'Body' no esté vacío
-		if todo.Body == "" { // Usar 'Body' con mayúscula
-			return c.Status(400).JSON(fiber.Map{"error": "todo body is required"})
-		}
+	collection = client.Database("golang_db").Collection("todos")
 
-		// Asignar un ID único al nuevo ToDo
-		todo.ID = len(todos) + 1
-		todos = append(todos, *todo)
+	app := fiber.New()
 
-		// Responder con el ToDo recién creado
-		return c.Status(201).JSON(todo)
-	})
+	app.Get("/api/todos", getTodos)
+	//app.Post("/api/todos", createTodos)
+	//app.Patch("/api/todos/:id", updateTodos)
+	//app.Delete("/api/todos/:id", deleteTodos)
 
-	// Ruta PATCH para actualizar un ToDo por ID
-	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
-		id, err := c.ParamsInt("id") // Obtener el ID del parámetro en la URL
-		if err != nil {
-			return c.Status(400).JSON(fiber.Map{"error": "invalid ID"})
-		}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
 
-		// Buscar el ToDo por ID
-		for i, todo := range todos {
-			if todo.ID == id { // Comparar ID
-				// Actualizar el ToDo (marcarlo como completado)
-				todos[i].Completed = true
-				return c.Status(200).JSON(todos[i]) // Retornar el ToDo actualizado
-			}
-		}
-
-		// Si no se encuentra el ToDo
-		return c.Status(404).JSON(fiber.Map{"error": "todo not found"})
-	})
-
-	//Delete a Todo
-
-	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
-
-		for i, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				todos = append(todos[:i], todos[i+1:]...)
-				return c.Status(200).JSON(fiber.Map{"success": true})
-			}
-		}
-
-		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
-	})
-
-	// Iniciar el servidor en el puerto 4000
-	log.Fatal(app.Listen(":" + PORT))
+	log.Fatal(app.Listen("0.0.0.0:" + port))
 }
+
+func getTodos(c *fiber.Ctx) error {}
+
+// func createTodos(c *fiber.Ctx) error {}
+
+// func updateTodos(c *fiber.Ctx) error {}
+
+// func deleteTodos(c *fiber.Ctx) error {}
