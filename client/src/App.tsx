54acfa54d2a5aa73a526f5322caff1ae8fb5fc4c
Paulina282000 <paulina.282000@gmail.com>
2025-04-02 00:00:17 -0300

    error en el main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,15 @@ import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
 
 
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
 function App() {
   return (
     <Stack h="100vh">
       <Navbar />
       <Container>
-         <TodoForm />
+        <TodoForm />
+        
       </Container>
     </Stack>
   );
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-// Renderizando la aplicación con Chakra UI
+// Si no necesitas personalizar, puedes usar el tema predeterminado
+const theme = extendTheme({});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
